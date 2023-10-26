22db8ac41d18506950fdc8bdac115e602b2bbe4c
cieloazul310 <cieloazul310@gmail.com>
2023-10-26 16:21:05 +0900

    feat: add geolocation

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@ import Map from "ol/Map";
 import View from "ol/View";
 import { fromLonLat } from "ol/proj";
 import { Attribution, ScaleLine, defaults as defaultControl } from "ol/control";
+import Link from "ol/interaction/Link";
+import { geolocation, useGeolocation, GeolocationControl } from "./utils";
 import gsiOptVtLayer from "./layers/gsi-opt-vt";
-
-import { parseHash, setPermalink, setPopstate } from "./utils/handleHash";
 import "./style.css";
 
-const { zoom, center, rotation } = parseHash(window);
+const geolocationControl = new GeolocationControl({ geolocation });
 
 const map = new Map({
   target: "map",
   view: new View({
-    center: center || fromLonLat([140.46, 36.37]),
-    zoom: zoom || 12,
-    rotation: rotation || 0,
+    center: fromLonLat([140.46, 36.37]),
+    zoom: 12,
+    rotation: 0,
   }),
   layers: [gsiOptVtLayer],
   controls: defaultControl({
@@ -24,8 +24,15 @@ const map = new Map({
       collapsible: false,
     }),
     new ScaleLine(),
+    geolocationControl,
   ]),
 });
 
-setPermalink(map);
-setPopstate(map, window);
+map.addInteraction(
+  new Link({
+    params: ["x", "y", "z"],
+    replace: true,
+  }),
+);
+
+useGeolocation({ map, geolocation });
